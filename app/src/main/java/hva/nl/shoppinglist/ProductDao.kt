b0e5699c4f64416df8834de304adc8bd368622b2
Luke <spaansl1@hva.nl>
2019-09-30 11:30:01 +0200

    Initial commit

---
+++ app/src/main/java/hva/nl/shoppinglist/ProductDao.kt
@@ -0,0 +1,18 @@
+package hva.nl.shoppinglist
+
+import androidx.room.*
+
+@Dao
+interface ProductDao {
+    @Query("SELECT * FROM product_table")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM product_table")
+    suspend fun deleteAllProducts()
+}
